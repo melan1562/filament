97d23f9b12144c7d332ac9683d4b16a9ef0c592f
melan1562 <melan_1562@yahoo.com>
2024-07-13 20:32:51 +0800

    update

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -5,21 +5,24 @@ namespace App\Filament\Resources;
 use App\Filament\Resources\UserResource\Pages;
 use App\Filament\Resources\UserResource\RelationManagers;
 use App\Models\User;
-use Filament\Forms\Components\TextInput;
-use Filament\Tables\Columns\TextColumn;
 use Filament\Forms;
 use Filament\Forms\Form;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Select;
 use Filament\Resources\Resource;
 use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+use Illuminate\Validation\Rules\Password;
 
 class UserResource extends Resource
 {
     protected static ?string $model = User::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-s-user-group';
 
     public static function form(Form $form): Form
     {
@@ -30,6 +33,11 @@ class UserResource extends Resource
                 // TextInput::make('password')->password()->visibleOn('create')  //visible, visibleOn
                 // TextInput::make('password')->password()->readOnlyOn('edit'), //readOnlyOn, readOnly
                 TextInput::make('password')->password(),
+                Select::make('gender')
+                    ->options([
+                    'male' => 'Male',
+                    'female' => 'Female',
+                ])
 
             ]);
     }
